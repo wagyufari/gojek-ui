6fdbf00b2ddd6702e7a99e99b845cd5db771a876
Muhammad Ghifari <ghifari9567@gmail.com>
2020-10-30 21:21:45 +0700

    Remove residue

--- app/src/main/java/com/mayburger/gojekuiclone/ui/pay/PaySuccessNavigator.kt
+++
@@ -1,8 +0,0 @@
-package com.mayburger.gojekuiclone.ui.pay
-
-import androidx.fragment.app.Fragment
-import com.mayburger.gojekuiclone.ui.base.BaseNavigator
-
-interface PaySuccessNavigator:BaseNavigator{
-    fun onBackPressed(fragment:Fragment)
-}
